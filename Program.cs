2d4c2318334e206c22824024df3e562a07fa8e30
maxmulla23 <maxmulla23@gmail.com>
2023-08-24 17:47:40 +0300

    get pizza items

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
+using PizzaStore.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<PizzaDB>(options => options.UseInMemoryDatabase("items"));
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1",new OpenApiInfo {
@@ -21,5 +23,6 @@ app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/Swagger/v1/swagger.json", "PizzaStore API V1");
 });
+app.MapGet("/pizzas", async (PizzaDB db) => await db.Pizzas.ToListAsync());
 
 app.Run();

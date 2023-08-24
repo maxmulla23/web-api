0359edd6607ebd7fb8870ea8498c00a36d95993f
maxmulla23 <maxmulla23@gmail.com>
2023-08-24 18:31:00 +0300

    sqlite added

--- Program.cs
+++ Program.cs
@@ -4,9 +4,10 @@ using PizzaStore.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Pizzas") ?? "Data Source=Pizzas.db";
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<PizzaDB>(options => options.UseInMemoryDatabase("items"));
+builder.Services.AddSqlite<PizzaDB>(connectionString);
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1",new OpenApiInfo {

36b6c87b9dcabfbc73b8fe521ad7f35e6c94d582
maxmulla23 <maxmulla23@gmail.com>
2023-08-24 17:35:24 +0300

    added entity framework

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 using Microsoft.OpenApi.Models;
-using PizzaStore.DB;
+using Microsoft.EntityFrameworkCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1",new OpenApiInfo { Title = "PizzaStore API", Description = "Making the Pizzas you love", Version= "v1" });
+    c.SwaggerDoc("v1",new OpenApiInfo {
+          Title = "PizzaStore API",
+          Description = "Making the Pizzas you love", 
+          Version = "v1" });
 
 });
 
@@ -17,10 +21,5 @@ app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/Swagger/v1/swagger.json", "PizzaStore API V1");
 });
-app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetPizza(id));
-app.MapGet("/pizzas", () => PizzaDB.GetPizzas());
-app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
-app.MapPut("/pizzas", (Pizza pizza) => PizzaDB.UpdatePizza(pizza));
-app.MapDelete("/pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));
 
 app.Run();
